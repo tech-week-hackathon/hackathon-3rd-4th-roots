6b2ec42643924e0ef6c8e0a59db0a6b293d80470
esis8 <eesis.dev@gmail.com>
2024-12-01 16:26:44 -0300

    update ui

--- src/pages/candidature/index.tsx
+++ src/pages/candidature/index.tsx
@@ -5,18 +5,14 @@ import { useEffect, useState } from "react";
 import { CandidateEntity } from "@example/src/lib/SmartDB/Entities";
 import { formatAddressUI } from "@example/src/utils/formats";
 
-
 export default function index() {
   const [candidates, setCandidates] = useState<CandidateEntity[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
 
-
   useEffect(() => {
     const fetchCandidates = async () => {
-
       try {
-        const result: CandidateEntity[] =
-          await CandidateApi.getAllApi_();
+        const result: CandidateEntity[] = await CandidateApi.getAllApi_();
         setCandidates(result);
       } catch (error) {
         console.error("Error fetching dreps:", error);
@@ -31,10 +27,7 @@ export default function index() {
     return <div className={styles.page}>Loading...</div>;
   }
 
-
   return (
-
-
     <>
       <Head>
         <title>Candidates | dExpo</title>
@@ -43,19 +36,26 @@ export default function index() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={styles.page}>
-        <h1 className={styles.title}>Welcome to dExpo</h1>
-        <h2 className={styles.subtitle}>Active Committees</h2>
-        <div className={styles.committeeList}>
-          {candidates.map((candidate) => {
-            return (
-              <div key={candidate._DB_id} className={styles.candidateItem}>
-                <p className={styles.text}>
-                  {formatAddressUI(candidate.address)}
-                </p>
-              </div>
-            );
-          })}
-        </div>
+        <h1 className={styles.title}>Candidate Proposals</h1>
+
+        {candidates.length > 0 ? (
+          <>
+            <h2 className={styles.subtitle}>Active Proposals</h2>
+            <div className={styles.committeeList}>
+              {candidates.map((candidate) => {
+                return (
+                  <div key={candidate._DB_id} className={styles.candidateItem}>
+                    <p className={styles.text}>
+                      {formatAddressUI(candidate.address)}
+                    </p>
+                  </div>
+                );
+              })}
+            </div>
+          </>
+        ) : (
+          <h2 className={styles.subtitle}>No proposals yet</h2>
+        )}
       </div>
     </>
   );
